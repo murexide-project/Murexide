0c2ba1741e80e1539ffbe2df3fb190ba171a2ae0
shijuhao <173592061+shijuhao@users.noreply.github.com>
2026-09-06 18:30:52 +0800

    opt: 恢复头像组件

--- app/src/main/java/com/juhao/murexide/ui/components/Avatar.kt
+++ app/src/main/java/com/juhao/murexide/ui/components/Avatar.kt
@@ -2,7 +2,8 @@ package com.juhao.murexide.ui.components
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -14,7 +15,6 @@ import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.juhao.murexide.ui.theme.UiState
 
-@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun Avatar(
     modifier: Modifier = Modifier,
@@ -26,12 +26,13 @@ fun Avatar(
     val context = LocalContext.current
     val squareAvatar by UiState.squareAvatar
     
-    val shape = if (squareAvatar && !alwaysCircle) {
-        MaterialShapes.Square.toShape()
-    } else {
-        MaterialShapes.Circle.toShape()
+    val shape = remember(squareAvatar) {
+        if (squareAvatar && !alwaysCircle) {
+            RoundedCornerShape(size / 4)
+        } else {
+            CircleShape
+        }
     }
-
     val imageRequest = remember(url) {
         ImageRequest.Builder(context)
             .data(url)
